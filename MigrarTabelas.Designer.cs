ebab42d867424db8daecee761480619924696837
Cristiano Grobério <34129366+djcristianosgp@users.noreply.github.com>
2024-11-26 16:33:22 -0300

    Inicio

---
+++ MigrarTabelas.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ExportFirebirdToSqlite
+{
+    partial class MigrarTabelas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MigrarTabelas));
+            pbResgistros = new ProgressBar();
+            Executar = new Button();
+            txtStringSQLITE = new TextBox();
+            txtStringFirebird = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            txtTabelas = new TextBox();
+            lblProgressoRegistros = new Label();
+            lblProgressoTabela = new Label();
+            pbTabelas = new ProgressBar();
+            SuspendLayout();
+            // 
+            // pbResgistros
+            // 
+            pbResgistros.Dock = DockStyle.Bottom;
+            pbResgistros.Location = new Point(0, 230);
+            pbResgistros.Name = "pbResgistros";
+            pbResgistros.Size = new Size(717, 34);
+            pbResgistros.TabIndex = 15;
+            // 
+            // Executar
+            // 
+            Executar.Location = new Point(12, 136);
+            Executar.Name = "Executar";
+            Executar.Size = new Size(179, 23);
+            Executar.TabIndex = 14;
+            Executar.Text = "Executar";
+            Executar.UseVisualStyleBackColor = true;
+            Executar.Click += Executar_Click;
+            // 
+            // txtStringSQLITE
+            // 
+            txtStringSQLITE.Location = new Point(12, 65);
+            txtStringSQLITE.Name = "txtStringSQLITE";
+            txtStringSQLITE.Size = new Size(668, 23);
+            txtStringSQLITE.TabIndex = 12;
+            txtStringSQLITE.Text = "Data Source=C:\\RGSystem\\BD\\DadosDevexpress.sqlite;Version=3;";
+            // 
+            // txtStringFirebird
+            // 
+            txtStringFirebird.Location = new Point(12, 21);
+            txtStringFirebird.Name = "txtStringFirebird";
+            txtStringFirebird.Size = new Size(668, 23);
+            txtStringFirebird.TabIndex = 11;
+            txtStringFirebird.Text = "Server=localhost;Database=VARGEMALTALOCAL;User=SYSDBA;Password=masterkey;";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 89);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Tabelas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 47);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 9;
+            label2.Text = "String Sqlite";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 5);
+            label1.Name = "label1";
+            label1.Size = new Size(81, 15);
+            label1.TabIndex = 8;
+            label1.Text = "String Firebird";
+            // 
+            // txtTabelas
+            // 
+            txtTabelas.Location = new Point(12, 107);
+            txtTabelas.Name = "txtTabelas";
+            txtTabelas.Size = new Size(668, 23);
+            txtTabelas.TabIndex = 16;
+            txtTabelas.Text = resources.GetString("txtTabelas.Text");
+            // 
+            // lblProgressoRegistros
+            // 
+            lblProgressoRegistros.Dock = DockStyle.Bottom;
+            lblProgressoRegistros.Location = new Point(0, 215);
+            lblProgressoRegistros.Name = "lblProgressoRegistros";
+            lblProgressoRegistros.Size = new Size(717, 15);
+            lblProgressoRegistros.TabIndex = 17;
+            lblProgressoRegistros.Text = "Progresso Registros";
+            // 
+            // lblProgressoTabela
+            // 
+            lblProgressoTabela.Dock = DockStyle.Bottom;
+            lblProgressoTabela.Location = new Point(0, 166);
+            lblProgressoTabela.Name = "lblProgressoTabela";
+            lblProgressoTabela.Size = new Size(717, 15);
+            lblProgressoTabela.TabIndex = 19;
+            lblProgressoTabela.Text = "ProgressoTabelas";
+            // 
+            // pbTabelas
+            // 
+            pbTabelas.Dock = DockStyle.Bottom;
+            pbTabelas.Location = new Point(0, 181);
+            pbTabelas.Name = "pbTabelas";
+            pbTabelas.Size = new Size(717, 34);
+            pbTabelas.TabIndex = 18;
+            // 
+            // MigrarTabelas
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(717, 264);
+            Controls.Add(lblProgressoTabela);
+            Controls.Add(pbTabelas);
+            Controls.Add(lblProgressoRegistros);
+            Controls.Add(txtTabelas);
+            Controls.Add(pbResgistros);
+            Controls.Add(Executar);
+            Controls.Add(txtStringSQLITE);
+            Controls.Add(txtStringFirebird);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "MigrarTabelas";
+            Text = "MigrarTabelas";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar pbResgistros;
+        private Button Executar;
+        private TextBox txtStringSQLITE;
+        private TextBox txtStringFirebird;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox txtTabelas;
+        private Label lblProgressoRegistros;
+        private Label lblProgressoTabela;
+        private ProgressBar pbTabelas;
+    }
+}
---
+++ MigrarTabelas.cs
@@ -0,0 +1,229 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using System.Data;
+using System.Data.SQLite;
+
+namespace ExportFirebirdToSqlite
+{
+    public partial class MigrarTabelas : Form
+    {
+        public MigrarTabelas()
+        {
+            InitializeComponent();
+        }
+
+        public async Task MigrateTables(string firebirdConnectionString, string sqliteConnectionString,
+                                     List<string> tableNames, ProgressBar tableProgressBar, ProgressBar recordProgressBar,
+                                     Label tableProgressLabel, Label recordProgressLabel)
+        {
+            try
+            {
+                using (var firebirdConnection = new FbConnection(firebirdConnectionString))
+                {
+                    firebirdConnection.Open();
+
+                    using (var sqliteConnection = new SQLiteConnection(sqliteConnectionString))
+                    {
+                        sqliteConnection.Open();
+
+                        // Configurar barra de progresso e label para tabelas
+                        tableProgressBar.Minimum = 0;
+                        tableProgressBar.Maximum = tableNames.Count;
+                        tableProgressBar.Value = 0;
+
+                        foreach (var tableName in tableNames)
+                        {
+
+                            // Atualizar label de progresso de tabelas
+                            tableProgressLabel.Text = $"Tabela: {tableName} - {(tableProgressBar.Value * 100) / tableProgressBar.Maximum}%";
+                            tableProgressLabel.Refresh();
+
+                            // Obter esquema da tabela Firebird
+                            string query = $"SELECT * FROM {tableName}";
+                            using (var firebirdCommand = new FbCommand(query, firebirdConnection))
+                            using (var firebirdReader = firebirdCommand.ExecuteReader())
+                            {
+                                var schemaTable = firebirdReader.GetSchemaTable();
+
+                                // Criar tabela ou adicionar campos no SQLite
+                                using (var sqliteCommand = new SQLiteCommand())
+                                {
+                                    sqliteCommand.Connection = sqliteConnection;
+
+                                    if (!TableExists(sqliteConnection, tableName))
+                                    {
+                                        // Criação da tabela no SQLite
+                                        string createTableSql = $"CREATE TABLE {tableName} (";
+                                        foreach (DataRow row in schemaTable.Rows)
+                                        {
+                                            string columnName = row["ColumnName"].ToString();
+                                            string dataType = row["DataType"].ToString();
+
+                                            string sqliteType = dataType switch
+                                            {
+                                                "System.Int32" => "INTEGER",
+                                                "System.String" => "TEXT",
+                                                "System.DateTime" => "DATETIME",
+                                                "System.Decimal" => "REAL",
+                                                "System.Double" => "REAL",
+                                                _ => "TEXT"
+                                            };
+
+                                            createTableSql += $"{columnName} {sqliteType}, ";
+                                        }
+
+                                        createTableSql = createTableSql.TrimEnd(',', ' ') + ");";
+                                        sqliteCommand.CommandText = createTableSql;
+                                        sqliteCommand.ExecuteNonQuery();
+                                    }
+                                    else
+                                    {
+                                        // Adicionar campos que ainda não existem
+                                        foreach (DataRow row in schemaTable.Rows)
+                                        {
+                                            string columnName = row["ColumnName"].ToString();
+                                            if (!ColumnExists(sqliteConnection, tableName, columnName))
+                                            {
+                                                string dataType = row["DataType"].ToString();
+
+                                                string sqliteType = dataType switch
+                                                {
+                                                    "System.Int32" => "INTEGER",
+                                                    "System.String" => "TEXT",
+                                                    "System.DateTime" => "DATETIME",
+                                                    "System.Decimal" => "REAL",
+                                                    "System.Double" => "REAL",
+                                                    _ => "TEXT"
+                                                };
+
+                                                string alterTableSql = $"ALTER TABLE {tableName} ADD COLUMN {columnName} {sqliteType};";
+                                                sqliteCommand.CommandText = alterTableSql;
+                                                sqliteCommand.ExecuteNonQuery();
+                                            }
+                                        }
+                                    }
+                                }
+
+                                // Configurar barra de progresso e label para registros
+                                int totalRecords = GetRowCount(firebirdConnection, tableName);
+                                recordProgressBar.Minimum = 0;
+                                recordProgressBar.Maximum = totalRecords;
+                                recordProgressBar.Value = 0;
+
+                                recordProgressLabel.Text = "0%";
+                                recordProgressLabel.Refresh();
+
+                                // Inserir dados no SQLite somente se não existirem
+                                using (var transaction = sqliteConnection.BeginTransaction())
+                                {
+                                    int processedRecords = 0;
+
+                                    while (firebirdReader.Read())
+                                    {
+                                        // Construir comando de verificação
+                                        string checkExistsSql = $"SELECT 1 FROM {tableName} WHERE ";
+                                        for (int i = 0; i < firebirdReader.FieldCount; i++)
+                                        {
+                                            string columnName = firebirdReader.GetName(i);
+                                            var value = firebirdReader.IsDBNull(i) ? "NULL" : $"'{firebirdReader.GetValue(i).ToString().Replace("'", "''")}'";
+                                            checkExistsSql += $"{columnName} = {value} AND ";
+                                        }
+                                        checkExistsSql = checkExistsSql.TrimEnd(" AND ".ToCharArray());
+
+                                        using (var checkCommand = new SQLiteCommand(checkExistsSql, sqliteConnection))
+                                        {
+                                            bool recordExists = checkCommand.ExecuteScalar() != null;
+
+                                            if (!recordExists)
+                                            {
+                                                string insertSql = $"INSERT INTO {tableName} VALUES (";
+                                                for (int i = 0; i < firebirdReader.FieldCount; i++)
+                                                {
+                                                    var value = firebirdReader.IsDBNull(i)
+                                                        ? "NULL"
+                                                        : $"'{firebirdReader.GetValue(i).ToString().Replace("'", "''")}'";
+                                                    insertSql += value + ", ";
+                                                }
+
+                                                insertSql = insertSql.TrimEnd(',', ' ') + ");";
+
+                                                using (var sqliteCommand = new SQLiteCommand(insertSql, sqliteConnection, transaction))
+                                                {
+                                                    sqliteCommand.ExecuteNonQuery();
+                                                }
+                                            }
+                                        }
+
+                                        // Atualizar barra de progresso de registros
+                                        processedRecords++;
+                                        recordProgressBar.Value = processedRecords;
+                                        recordProgressBar.Refresh();
+
+                                        // Atualizar label de progresso de registros
+                                        recordProgressLabel.Text = $"Registros: {(processedRecords * 100) / totalRecords}%";
+                                        recordProgressLabel.Refresh();
+                                    }
+
+                                    transaction.Commit();
+                                }
+                            }
+
+                            // Atualizar barra de progresso de tabelas
+                            tableProgressBar.Value++;
+                            tableProgressBar.Refresh();
+
+                            // Atualizar label de progresso de tabelas
+                            tableProgressLabel.Text = $"Tabela: {tableName} - {(tableProgressBar.Value * 100) / tableProgressBar.Maximum}%";
+                            tableProgressLabel.Refresh();
+                        }
+                    }
+
+                    MessageBox.Show("Migração concluída com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro na migração: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static bool TableExists(SQLiteConnection connection, string tableName)
+        {
+            string query = "SELECT name FROM sqlite_master WHERE type='table' AND name=@tableName";
+            using (var command = new SQLiteCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@tableName", tableName);
+                return command.ExecuteScalar() != null;
+            }
+        }
+
+        private static bool ColumnExists(SQLiteConnection connection, string tableName, string columnName)
+        {
+            string query = $"PRAGMA table_info({tableName});";
+            using (var command = new SQLiteCommand(query, connection))
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    if (reader["name"].ToString().Equals(columnName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private static int GetRowCount(FbConnection connection, string tableName)
+        {
+            string countQuery = $"SELECT COUNT(*) FROM {tableName}";
+            using (var command = new FbCommand(countQuery, connection))
+            {
+                return Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+        private async void Executar_Click(object sender, EventArgs e)
+        {
+            var lista = new List<string>(txtTabelas.Text.ToUpper().Split(','));
+            await MigrateTables(txtStringFirebird.Text, txtStringSQLITE.Text, lista, pbTabelas, pbResgistros, lblProgressoTabela, lblProgressoRegistros);
+        }
+    }
+}
